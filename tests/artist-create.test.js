f861eeef5e278161f9578ddb04d6fc7db852ac8b
Vee Uye <vee.uye@gmail.com>
2022-04-27 14:17:01 +0100

    add test file artist-create

---
+++ tests/artist-create.test.js
@@ -0,0 +1,20 @@
+const { expect } = require('chai');
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('create artist', () => {
+  describe('/artist', () => {
+    describe('POST', () => {
+      it('creates a new artist in the database', async () => {
+        const res = await (
+          await request(app).post('/artist')
+        ).setEncoding({
+          name: 'Tame Impala',
+          genre: 'rock',
+        });
+
+        expect(res.status).to.equal(201);
+      });
+    });
+  });
+});
