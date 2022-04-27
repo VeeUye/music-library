36f496c04c93eb9b8752405e7b229ca3864c930e
Vee Uye <vee.uye@gmail.com>
2022-04-27 16:15:33 +0100

    update artist-create test file

--- tests/artist-create.test.js
+++ tests/artist-create.test.js
@@ -1,8 +1,16 @@
 const { expect } = require('chai');
 const request = require('supertest');
+const getDb = require('../src/services/db');
 const app = require('../src/app');
 
 describe('create artist', () => {
+  let db;
+  beforeEach(async () => (db = await getDb()));
+  afterEach(async () => {
+    await db.query('DELETE FROM Artist');
+    await db.close();
+  });
+
   describe('/artist', () => {
     describe('POST', () => {
       it('creates a new artist in the database', async () => {
@@ -12,6 +20,13 @@ describe('create artist', () => {
         });
 
         expect(res.status).to.equal(201);
+
+        const [[artistEntries]] = await db.query(
+          `SELECT * FROM Artist WHERE name = 'Tame Impala'`
+        );
+
+        expect(artistEntries.name).to.equal('Tame Impala');
+        expect(artistEntries.genre).to.equal('rock');
       });
     });
   });
