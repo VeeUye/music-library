2e670a34cb8d1ce97835db2ca3aec23138800d36
Vee Uye <vee.uye@gmail.com>
2022-04-27 21:13:53 +0100

    troubleshoot push

--- src/services/db.js
+++ src/services/db.js
@@ -1,6 +1,10 @@
 const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env.test' });
 
 const { DB_PASSWORD, DB_USER, DB_NAME, DB_HOST, DB_PORT } = process.env;
+
+console.log(DB_NAME);
 
 module.exports = async () => {
   const connection = await mysql.createConnection({
