68f1d96b74d3d521b24e4367d2294222e44e7ad0
Vee Uye <vee.uye@gmail.com>
2022-04-27 16:56:53 +0100

    add SQL statement to update Artists

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,3 +1,16 @@
-exports.createArtist = (req, res) => {
-  res.sendStatus(201);
+const getDb = require('../services/db');
+
+exports.createArtist = async (req, res) => {
+  const db = await getDb();
+  const { name, genre } = req.body;
+  try {
+    await db.query(
+      `INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`
+    );
+
+    res.sendStatus(201);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+  db.close();
 };
--- utils/create-database.js
+++ utils/create-database.js
@@ -32,6 +32,12 @@ const setUpDatabase = async () => {
 
     // create the database if it doesn't already exist
     await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
+    await db.query(`USE ${DB_NAME}`);
+    await db.query(`CREATE TABLE IF NOT EXISTS Artist (
+      id INT PRIMARY KEY auto_increment,
+      name VARCHAR(25),
+      genre VARCHAR(25)
+    )`);
     await db.end();
   } catch (err) {
     // if something goes wrong, console.log the error and the current environment variables
