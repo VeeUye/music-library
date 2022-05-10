0ece4e7f076fc3e94c4362b83012f77c7a9fdde8
Vee Uye <vee.uye@gmail.com>
2022-05-10 21:55:39 +0100

    create swagger.json and added to app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const artistRouter = require('./routes/artist');
 const albumRouter = require('./routes/album');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('../swagger.json');
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use('/artist', artistRouter);
 
