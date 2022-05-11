56e5570ec575a642714fe9bcfadf2c478b151408
Vee Uye <vee.uye@gmail.com>
2022-05-11 22:04:25 +0100

    remove comments

--- src/app.js
+++ src/app.js
@@ -7,15 +7,6 @@ const app = express();
 
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   );
-//   next();
-// });
-
 app.use('/artist', artistRouter);
 
 app.use('/album', albumRouter);
