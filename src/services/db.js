afaf89b8acb51e20db92f52fd573a73f3dbaefa9
Vee Uye <vee.uye@gmail.com>
2022-05-11 14:35:07 +0100

    Add deployment code

--- src/services/db.js
+++ src/services/db.js
@@ -2,18 +2,27 @@ const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 dotenv.config({ path: '.env.test' });
 
-const { DB_PASSWORD, DB_USER, DB_NAME, DB_HOST, DB_PORT } = process.env;
+const {
+  DB_PASSWORD,
+  DB_USER,
+  DB_NAME,
+  DB_HOST,
+  DB_PORT,
+  CLEARDB_DATABASE_URL,
+} = process.env;
 
 console.log(DB_NAME);
 
 module.exports = async () => {
-  const connection = await mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    port: DB_PORT,
-    database: DB_NAME,
-  });
+  const connection = CLEARDB_DATABASE_URL
+    ? await mysql.createConnection(CLEARDB_DATABASE_URL)
+    : await mysql.createConnection({
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        port: DB_PORT,
+        database: DB_NAME,
+      });
 
   return connection;
 };
--- utils/create-database.js
+++ utils/create-database.js
@@ -3,23 +3,50 @@ const path = require('path');
 const args = process.argv.slice(2)[0];
 const envFile = args === 'test' ? '../.env.test' : '../.env';
 
-require('dotenv').config({
-  path: path.join(__dirname, envFile),
-});
+// require('dotenv').config({
+//   path: path.join(__dirname, envFile),
+// });
+if (args === 'test') {
+  require('dotenv').config({
+    path: path.join(__dirname, envFile),
+  });
+}
 
-const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
+const {
+  DB_PASSWORD,
+  DB_NAME,
+  DB_USER,
+  DB_HOST,
+  DB_PORT,
+  CLEARDB_DATABASE_URL,
+} = process.env;
 
 const setUpDatabase = async () => {
   try {
-    const db = await mysql.createConnection({
-      host: DB_HOST,
-      user: DB_USER,
-      password: DB_PASSWORD,
-      port: DB_PORT,
-    });
+    const db = CLEARDB_DATABASE_URL
+      ? await mysql.createConnection(CLEARDB_DATABASE_URL)
+      : await mysql.createConnection({
+          host: DB_HOST,
+          user: DB_USER,
+          password: DB_PASSWORD,
+          port: DB_PORT,
+          database: DB_NAME,
+        });
 
-    await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
-    await db.query(`USE ${DB_NAME}`);
+    // const db = await mysql.createConnection({
+    //   host: DB_HOST,
+    //   user: DB_USER,
+    //   password: DB_PASSWORD,
+    //   port: DB_PORT,
+    // });
+
+    // await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
+    // await db.query(`USE ${DB_NAME}`);
+
+    !CLEARDB_DATABASE_URL &&
+      (await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`));
+    !CLEARDB_DATABASE_URL && (await db.query(`USE ${DB_NAME}`));
+
     await db.query(`CREATE TABLE IF NOT EXISTS Artist (
       id INT PRIMARY KEY auto_increment,
       name VARCHAR(25),
