c3b2ff481b38c243144c2d9ba730a87a6f8d67b9
Vee Uye <vee.uye@gmail.com>
2022-05-11 15:19:53 +0100

    display text on app open

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const app = require('./src/app.js');
 
 const APP_PORT = process.env.PORT || 4000;
 
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
 app.listen(APP_PORT, () => {
   console.log(`App is listening on port ${APP_PORT}`);
 });
